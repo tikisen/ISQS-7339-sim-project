0cee79cda57e6a12af972450d5cec854b8b124bb
Corey <64378683+coreyshank@users.noreply.github.com>
2020-04-28 20:18:07 -0500

    Update ISQS-7339-sim-project-case-1.py
    Edited with calling the real Objective function as the real objective function

--- Case 1/ISQS-7339-sim-project-case-1.py
+++ Case 1/ISQS-7339-sim-project-case-1.py
@@ -9,13 +9,13 @@ from pulp import *
 
 import math
 
-MonthBudg = 99999999 #Monthly Budget
+MonthBudg = 9500 #Monthly Budget
 
-SGICost = 9000 #9000 is discounted cost in Months 1 and 2
+SGICost = 10000 #9000 is discounted cost in Months 1 and 2
 
-SunCost = 18750 #18750 is the discounted cost in Months 1 and 2
+SunCost = 20000 #18750 is the discounted cost in Months 1 and 2
 
-alpha = 10000
+alpha = 100000
 
 # Server Capacity
 ServerCap = {'1_Std-Pentium-Server':30, '2_Enh-Pentium-Server':80, '3_SGI-Server':200, '4_Sun-Server':2000}
@@ -38,12 +38,13 @@ prob = LpProblem("Case 1 Part A", LpMinimize)
 x = LpVariable.dicts('x', ServerCt, lowBound=0, cat=pulp.LpInteger)
 
 # Objective
-prob += sum(ServerCost[i]*x[i] for i in ServerCt)
+#prob += sum(ServerCost[i]*x[i] for i in ServerCt)
+prob += sum(ServerCost[i]*x[i]*alpha for i in ServerCt) - sum(ServerCap[i]*x[i] for i in ServerCt),'Objective'
 
 # Constraint
 prob += sum(ServerCap[i]*x[i] for i in ServerCt) >= Staff
 prob += sum(ServerCost[i]*x[i] for i in ServerCt) <= MonthBudg
-prob += sum(ServerCost[i]*x[i]*alpha for i in ServerCt) - sum(ServerCap[i]*x[i] for i in ServerCt)
+
 
 # Quantity of each Server:
 for i in ServerCt:
